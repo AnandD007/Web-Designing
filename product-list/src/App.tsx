5a3e720a761e50d23fd87077210830510ab3ca51
Anand-Dev <64485733+AnandD007@users.noreply.github.com>
2023-04-13 18:56:21 +0530

    Added Modified Assignment - 5 - Web Design

---
+++ product-list/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import ProductsList from './Productlist';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductsList />
+    </div>
+  );
+}
+
+export default App;
