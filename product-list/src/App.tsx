bcf5de166e43772263f9e2a021e030010e551ce3
Anand-Dev <64485733+AnandD007@users.noreply.github.com>
2023-04-14 16:41:51 +0530

    Added All changes

---
+++ product-list/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import ProductsList from './components/productList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductsList />
+    </div>
+  );
+}
+
+export default App;
